fbb3f6b102144f5c617f25c37d24c70f63ad844a
Mikkel Paulson <git@email.mikkel.ca>
2021-02-28 17:21:09 -0500

    impl template parser script

--- scripts/src/main.rs
+++ scripts/src/main.rs
@@ -1,18 +1,21 @@
-//use json::JsonValue;
+use json::JsonValue;
 use quick_xml::events::Event;
 use quick_xml::Reader;
 use regex::Regex;
 use std::collections::HashMap;
+use std::fmt;
+use std::fs::File;
 use std::io;
 use std::str::FromStr;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = Vec::new();
-    //let mut json = JsonValue::new_object();
+    let mut json_objects: HashMap<InfoboxType, JsonValue> = HashMap::new();
     let stdin = io::stdin();
     let mut reader = Reader::from_reader(stdin.lock());
     let mut read_mode = None;
     let mut skip_page = false;
+    let mut title = String::new();
 
     loop {
         match reader.read_event(&mut buf) {
@@ -20,6 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 read_mode = match e.name() {
                     b"page" => {
                         skip_page = false;
+                        title = String::new();
                         None
                     }
                     b"title" => Some(ReadMode::Title),
@@ -33,22 +37,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             Ok(Event::Text(e)) => {
                 match read_mode {
                     Some(ReadMode::Title) => {
-                        let title = e.unescape_and_decode(&reader)?;
+                        title = e.unescape_and_decode(&reader)?;
                         if title.contains(':') {
                             skip_page = true;
                         }
                     }
                     Some(ReadMode::Text) if !skip_page => {
-                        println!("{:?}", parse(e.unescape_and_decode(&reader)?.as_str()));
+                        if let Some((infobox_type, fields)) =
+                            parse(e.unescape_and_decode(&reader)?.as_str())
+                        {
+                            json_objects
+                                .entry(infobox_type)
+                                .or_insert_with(|| JsonValue::new_object())
+                                .insert(title.as_str(), fields)
+                                .unwrap();
+                        }
                     }
                     _ => {}
                 }
                 read_mode = None;
             }
-            Ok(Event::End(_)) => break,
+            Ok(Event::Eof) => break,
             _ => (),
         }
         buf.clear();
+    }
+
+    for (infobox_type, json_object) in json_objects.iter() {
+        json_object.write_pretty(&mut File::create(format!("{}s.json", infobox_type))?, 2)?;
     }
 
     Ok(())
@@ -73,8 +89,11 @@ fn parse(text: &str) -> Option<(InfoboxType, HashMap<&str, String>)> {
             infobox_type = caps.name("name").unwrap().as_str().parse().ok();
             depth = Some(0);
         } else if let Some(caps) = field_start.captures(line) {
-            if !key.is_empty() && !value.is_empty() {
-                fields.insert(key, strip_tags(value.as_str()));
+            if !key.is_empty() {
+                value = strip_tags(value.as_str());
+                if !value.is_empty() {
+                    fields.insert(key, value);
+                }
             }
 
             key = caps.name("name").unwrap().as_str();
@@ -107,7 +126,7 @@ fn strip_tags(text: &str) -> String {
     let mut output = String::with_capacity(text.len());
     let mut reader = Reader::from_str(text);
     let mut tag_depth: u8 = 0;
-    let wikitext_pattern = Regex::new(r"\{\{|\}\}|\[\[|\]\]").unwrap();
+    let wikitext_pattern = Regex::new(r"\{\{|\}\}|\[\[|\]\]|\|").unwrap();
 
     loop {
         match reader.read_event(&mut buf) {
@@ -117,24 +136,33 @@ fn strip_tags(text: &str) -> String {
             Ok(Event::Text(e)) if tag_depth == 0 => {
                 let mut i = 0;
                 let mut template_depth: u8 = 0;
+                let mut link_depth: u8 = 0;
 
-                let text = e.unescape_and_decode(&reader).unwrap();
-                for caps in wikitext_pattern.captures_iter(&text) {
-                    let cap_match = caps.get(0).unwrap();
+                if let Ok(text) = e.unescape_and_decode(&reader) {
+                    for caps in wikitext_pattern.captures_iter(&text) {
+                        let cap_match = caps.get(0).unwrap();
 
-                    if template_depth == 0 {
-                        output.push_str(&text[i..cap_match.start()]);
+                        if template_depth == 0 && link_depth == 0 {
+                            output.push_str(&text[i..cap_match.start()]);
+                        }
+
+                        match cap_match.as_str() {
+                            "{{" => template_depth += 1,
+                            "}}" => template_depth = template_depth.saturating_sub(1),
+                            "[[" => link_depth += 1,
+                            "]]" => {
+                                link_depth = link_depth.saturating_sub(1);
+                                if link_depth == 0 {
+                                    output.push_str(&text[i..cap_match.start()]);
+                                }
+                            }
+                            _ => {}
+                        }
+
                         i = cap_match.end();
                     }
-
-                    match cap_match.as_str() {
-                        "{{" => template_depth += 1,
-                        "}}" => template_depth = template_depth.saturating_sub(1),
-                        _ => {}
-                    }
+                    output.push_str(&text[i..text.len()]);
                 }
-
-                output.push_str(&text[i..text.len()]);
             }
             Ok(Event::Eof) => break,
             _ => {}
@@ -150,7 +178,7 @@ enum ReadMode {
     Text,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 enum InfoboxType {
     Creature,
     Person,
@@ -171,5 +199,17 @@ impl FromStr for InfoboxType {
             "building" | "Building" => InfoboxType::Building,
             _ => Err(())?,
         })
+    }
+}
+
+impl fmt::Display for InfoboxType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            InfoboxType::Creature => write!(f, "creature"),
+            InfoboxType::Person => write!(f, "person"),
+            InfoboxType::State => write!(f, "state"),
+            InfoboxType::Location => write!(f, "location"),
+            InfoboxType::Building => write!(f, "building"),
+        }
     }
 }
