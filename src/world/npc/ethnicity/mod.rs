151e225a33e774edd15ee75f9ba16846a62f9859
Mikkel Paulson <git@email.mikkel.ca>
2021-03-25 10:53:27 -0400

    add ethnicity: Egyptian

--- src/world/npc/ethnicity/mod.rs
+++ src/world/npc/ethnicity/mod.rs
@@ -7,6 +7,7 @@ use super::{Age, Gender, Npc, Race};
 mod arabic;
 mod celtic;
 mod chinese;
+mod egyptian;
 mod warforged;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
@@ -83,6 +84,7 @@ pub fn regenerate(rng: &mut impl Rng, npc: &mut Npc) {
             Ethnicity::Arabic => arabic::Ethnicity::regenerate(rng, npc),
             Ethnicity::Celtic => celtic::Ethnicity::regenerate(rng, npc),
             Ethnicity::Chinese => chinese::Ethnicity::regenerate(rng, npc),
+            Ethnicity::Egyptian => egyptian::Ethnicity::regenerate(rng, npc),
             Ethnicity::Warforged => warforged::Ethnicity::regenerate(rng, npc),
             _ => {}
         }
