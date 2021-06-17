bbafd81f552dbe32e536e4e7e84d8b421f13613a
Mikkel Paulson <git@email.mikkel.ca>
2021-06-17 14:45:56 -0400

    rm wee_alloc feature
    Code size isn't presently a big priority, so removed the wee_alloc
    feature and its associated dependency.

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -4,12 +4,6 @@ use wasm_bindgen::prelude::*;
 
 use initiative_core::app;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn command(input: &str) -> String {
     let mut app = app();
