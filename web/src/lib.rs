9b28e9d5664755cfa25e75125805fe7a5841fc3b
Mikkel Paulson <git@email.mikkel.ca>
2021-07-10 16:15:28 -0400

    persist app in wasm
    Rather than reinitializing the app container on each command, persist
    the single container. This allows us to persist ephemeral context
    information between commands. (This was already the case for the CLI.)

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,11 +1,22 @@
 mod utils;
 
+use initiative_core as core;
 use wasm_bindgen::prelude::*;
-
-use initiative_core::app;
 
 #[wasm_bindgen]
 pub fn command(input: &str) -> String {
-    let mut app = app();
-    format!("{}", app.command(input))
+    format!("{}", app().command(input))
+}
+
+static mut APP: Option<core::app::App> = None;
+
+#[no_mangle]
+pub extern "C" fn app() -> &'static mut core::app::App {
+    unsafe {
+        if APP.is_none() {
+            APP = Some(core::app());
+        }
+
+        APP.as_mut().unwrap()
+    }
 }
