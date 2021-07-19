93c79f08d13675e1606536a084b04a74ff0a5466
Mikkel Paulson <git@email.mikkel.ca>
2021-07-19 13:13:37 -0400

    impl selecting and submitting suggestions
    BUG: Multiple return causes commands to re-run (might be a feature) but
    only if those commands are selected from the suggestions.
    
    TODO: Need styling for selected suggestions.

--- web/www/index.js
+++ web/www/index.js
@@ -5,31 +5,44 @@ const promptForm = document.getElementById("prompt-form");
 const prompt = document.getElementById("prompt");
 const output = document.getElementById("output");
 
-promptForm.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const input = prompt.value;
-
-  output.insertAdjacentHTML(
-    'beforeend',
-    "\n\n> " + input + "\n\n"
-  );
-
-  output.insertAdjacentHTML(
-    'beforeend',
-    wasm.command(prompt.value)
-  );
-
-  prompt.value = "";
-  window.scrollBy(0, window.innerHeight);
-});
-
 const autoCompleteJS = new autoComplete({
   selector: "#prompt",
   data: {
     src: async (query) => wasm.autocomplete(query),
   },
   submit: true,
+});
+
+const runCommand = command => {
+  output.insertAdjacentHTML(
+    'beforeend',
+    "\n\n> " + command + "\n\n"
+  );
+
+  output.insertAdjacentHTML(
+    'beforeend',
+    wasm.command(command)
+  );
+
+  prompt.value = "";
+  autoCompleteJS.close();
+  window.scrollBy(0, window.innerHeight);
+}
+
+promptForm.addEventListener("submit", event => {
+  event.preventDefault();
+  if (prompt.value !== "") {
+    runCommand(prompt.value);
+  }
+});
+
+promptForm.addEventListener("navigate", event => {
+  prompt.value = event.detail.selection.value;
+  console.log(event);
+});
+
+promptForm.addEventListener("selection", event => {
+  runCommand(event.detail.selection.value);
 });
 
 prompt.focus();
