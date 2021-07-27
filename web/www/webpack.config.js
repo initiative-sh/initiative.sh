7d99ec8fbf69b5a0f91524a61ba6e46201ff7f2c
Mikkel Paulson <git@email.mikkel.ca>
2021-07-27 10:49:54 -0400

    update to Webpack 5
    Totally mangled everything in an attempt to run the latest version of
    Webpack. Good news: this should also resolve an (irrelevant) security
    warning.

--- web/www/webpack.config.js
+++ web/www/webpack.config.js
@@ -1,14 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "index.js",
   },
   mode: "development",
+  /*
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  */
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/*.css" },
+      ],
+    }),
   ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
 };
