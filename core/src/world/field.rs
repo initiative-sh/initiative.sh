9a574a627b20eac74f2f82b32abecd00aef39b9e
Mikkel Paulson <git@email.mikkel.ca>
2021-08-02 11:57:20 -0400

    impl Serialize/Deserialize for Field
    The basic building block of the World, allowed Field<T> to be serialized
    and deserialized as if it were Option<T>. Honestly, the concept of Field
    may end up being dropped altogether.

--- core/src/world/field.rs
+++ core/src/world/field.rs
@@ -1,14 +1,16 @@
+use serde::{Deserialize, Serialize, Serializer};
 use std::fmt;
 use std::mem;
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Field<T: fmt::Display> {
+#[derive(Clone, Debug, Eq, PartialEq, Deserialize)]
+#[serde(from = "Option<T>")]
+pub enum Field<T> {
     Locked(T),
     Unlocked(T),
     Empty,
 }
 
-impl<T: fmt::Display> Field<T> {
+impl<T> Field<T> {
     pub fn new(value: T) -> Self {
         Self::Locked(value)
     }
@@ -97,19 +99,28 @@ impl<T: fmt::Display> Field<T> {
     }
 }
 
-impl<T: fmt::Display> Default for Field<T> {
+impl<T> Default for Field<T> {
     fn default() -> Self {
         Self::Empty
     }
 }
 
-impl<T: fmt::Display> From<T> for Field<T> {
+impl<T> From<T> for Field<T> {
     fn from(value: T) -> Field<T> {
         Self::new(value)
     }
 }
 
-impl<T: fmt::Display> From<Field<T>> for Option<T> {
+impl<T> From<Option<T>> for Field<T> {
+    fn from(value: Option<T>) -> Field<T> {
+        match value {
+            Some(v) => v.into(),
+            None => Field::default(),
+        }
+    }
+}
+
+impl<T> From<Field<T>> for Option<T> {
     fn from(field: Field<T>) -> Option<T> {
         match field {
             Field::Locked(value) => Some(value),
@@ -134,8 +145,20 @@ impl<T: fmt::Display> fmt::Display for Field<T> {
     }
 }
 
+impl<T: Serialize> Serialize for Field<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self.value() {
+            Some(v) => serializer.serialize_some(v),
+            None => serializer.serialize_none(),
+        }
+    }
+}
+
 #[cfg(test)]
-mod test_field {
+mod test {
     use super::Field;
 
     #[test]
@@ -209,5 +232,23 @@ mod test_field {
         let mut field: Field<_> = Field::new(123);
         field.clear();
         assert!(field.is_some());
+    }
+
+    #[test]
+    fn serialize_test() {
+        let field: Field<_> = Field::new(123);
+        assert_eq!("123", serde_json::to_string(&field).unwrap());
+
+        let field: Field<bool> = Field::default();
+        assert_eq!("null", serde_json::to_string(&field).unwrap());
+    }
+
+    #[test]
+    fn deserialize_test() {
+        let field: Field<u8> = serde_json::from_str("123").unwrap();
+        assert_eq!(Field::Locked(123), field);
+
+        let field: Field<u8> = serde_json::from_str("null").unwrap();
+        assert_eq!(Field::Empty, field);
     }
 }
