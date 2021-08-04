956035bb7e24e9460ac8133495877e4a7ccffedb
Mikkel Paulson <git@email.mikkel.ca>
2021-08-04 10:36:34 -0400

    impl saving to database
    This is it, folks. We can now dump data into the black hole that is the
    data store. Currently no way to read it back, however.
    
    Removing the WASM tests for now. They're not providing a whole lot of
    value, and would probably be better handled with something
    Selenium-based that can cover the JS side of things as well.

--- web/js/database.js
+++ web/js/database.js
@@ -1,4 +1,15 @@
-const save = async (thing) => console.log(thing);
+import Dexie from "dexie";
+
+const dexie = new Dexie("initiative");
+
+dexie.version(1).stores({
+  things: "&uuid, name, type",
+});
+
+const save = async (thing) => {
+  await dexie.things.put(thing);
+}
+
 const get_all = () => console.log(uuid);
 
 export { save, get_all };
