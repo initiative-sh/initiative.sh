e123989fdacd231a8a419bb73e6f9fefc183efc8
Mikkel Paulson <git@email.mikkel.ca>
2021-08-06 11:34:22 -0400

    DataStore::load() -> DataStore::get_all()
    Really, all we need is the ability to load all data from storage. We'll
    do our searching against local data.

--- core/src/storage/data_store.rs
+++ core/src/storage/data_store.rs
@@ -1,5 +1,4 @@
 use crate::world::Thing;
-use uuid::Uuid;
 
 #[derive(Default)]
 pub struct NullDataStore;
@@ -7,13 +6,13 @@ pub struct NullDataStore;
 impl DataStore for NullDataStore {
     fn save(&mut self, _thing: &Thing) {}
 
-    fn load(&self, _uuid: &Uuid) -> Option<Thing> {
-        None
+    fn get_all(&self) -> Vec<Thing> {
+        Vec::new()
     }
 }
 
 pub trait DataStore {
     fn save(&mut self, thing: &Thing);
 
-    fn load(&self, uuid: &Uuid) -> Option<Thing>;
+    fn get_all(&self) -> Vec<Thing>;
 }
