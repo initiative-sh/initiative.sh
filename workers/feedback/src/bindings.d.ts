184f918e31aa2d1af40155bb7b72dd61971a4a87
Mikkel Paulson <git@email.mikkel.ca>
2021-09-01 11:21:39 -0400

    refactor feedback worker into functions
    Refactor the big function of doom into something more comprehensible.

--- workers/feedback/src/bindings.d.ts
+++ workers/feedback/src/bindings.d.ts
@@ -2,5 +2,7 @@ export {}
 
 declare global {
   const GITHUB_TOKEN: string
+  const GITHUB_OWNER: string
+  const GITHUB_REPO: string
   const RATE_LIMIT: KVNamespace
 }
--- workers/feedback/src/handler.ts
+++ workers/feedback/src/handler.ts
@@ -1,10 +1,285 @@
 import { Octokit } from '@octokit/core'
 
-const OWNER = 'MikkelPaulson'
-const REPO = 'initiative.sh'
+class AppError {
+  debugMessage: string
+  response: Response
+
+  constructor(
+    debugMessage: string,
+    message = 'An error occurred. Please try contacting us by email: support@initiative.sh',
+    responseMeta: ResponseInit | null = null,
+  ) {
+    this.debugMessage = debugMessage
+    this.response = new Response(message, responseMeta ?? { status: 400 })
+  }
+}
+
+class PostData {
+  error: string | null
+  history: string | null
+  message: string
+  userAgent: string
+  userIP: string
+
+  constructor(
+    error: string | null,
+    history: string | null,
+    message: string,
+    userAgent: string,
+    userIP: string,
+  ) {
+    this.error = error
+    this.history = history
+    this.message = message
+    this.userAgent = userAgent
+    this.userIP = userIP
+  }
+
+  getBody(): string {
+    let issueBody = 'Message: ' + sanitizeInline(this.message)
+
+    if (this.error !== null) {
+      issueBody += '\nError: ' + sanitizeInline(this.error)
+    }
+
+    issueBody += '\nUser-agent: ' + sanitizeInline(this.userAgent)
+    issueBody += '\nUser IP: ' + sanitizeInline(this.userIP)
+
+    if (this.history !== null) {
+      issueBody += '\n\n---\n\n' + sanitizeBlock(this.history)
+    }
+
+    return issueBody
+  }
+}
 
 export async function handleRequest(request: Request): Promise<Response> {
-  const userIP = request.headers.get('x-real-ip') || ''
+  const userIP = request.headers.get('x-real-ip')
+
+  try {
+    await assertNotRateLimited(userIP)
+    return await dispatchRoutes(request)
+  } catch (e) {
+    if (e instanceof AppError) {
+      console.log('Error:', e.debugMessage)
+      return e.response
+    } else {
+      console.log('Error:', e)
+      return new Response('An error occurred. Please try again later.', {
+        status: 500,
+      })
+    }
+  }
+}
+
+async function dispatchRoutes(request: Request): Promise<Response> {
+  switch (request.url.replace(/^https?:\/\/[^/]+/, '')) {
+    case '/':
+      assertRequestMethod(request, 'POST')
+      return handlePostIndex(await parsePostIndexRequest(request))
+  }
+
+  return handle404(request)
+}
+
+async function handlePostIndex(postData: PostData): Promise<Response> {
+  console.log(JSON.stringify(postData))
+
+  if (postData.error !== null) {
+    return await handlePostIndexErrorReport(postData)
+  } else {
+    return await handlePostIndexSuggestion(postData)
+  }
+}
+
+async function parsePostIndexRequest(request: Request): Promise<PostData> {
+  assertContentType(request, 'application/json')
+
+  try {
+    const requestBody = await request.json()
+
+    if (requestBody === null || typeof requestBody.message !== 'string') {
+      throw new AppError('Request missing "message" field.', 'Invalid request.')
+    }
+
+    return new PostData(
+      requestBody.error ?? null,
+      requestBody.history ?? null,
+      requestBody.message,
+      request.headers.get('user-agent') ?? 'not provided',
+      request.headers.get('x-real-ip') ?? 'not provided',
+    )
+  } catch (e) {
+    if (typeof e === 'string') {
+      throw new AppError('JSON parse error: ' + e, 'Invalid request.')
+    } else {
+      throw e
+    }
+  }
+}
+
+/**
+ * Bug report - `report` command
+ */
+async function handlePostIndexErrorReport(
+  postData: PostData,
+): Promise<Response> {
+  const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+  const issueLabel = 'user error report'
+  const issueTitle = postData.error || 'Empty error message'
+
+  let issueNumber = null
+
+  try {
+    const query = `repo:${GITHUB_OWNER}/${GITHUB_REPO} is:issue is:open label:"${issueLabel}" "${sanitizeTitle(
+      issueTitle,
+    )}" in:title`
+    console.log('Searching for ' + query)
+
+    const searchResults = await octokit.request('GET /search/issues', {
+      accept: 'application/vnd.github.v3+json',
+      q: query,
+      sort: 'created',
+      per_page: 100,
+    })
+
+    if (searchResults.data.total_count > 0) {
+      for (const issue of searchResults.data.items) {
+        if (issue.title === issueTitle) {
+          issueNumber = issue.number
+          console.log(`Matched issue ${issueNumber}: ${issue.title}`)
+          break
+        }
+      }
+
+      if (issueNumber === null) {
+        console.log(
+          `No exact matches found among ${searchResults.data.total_count} results.`,
+        )
+      }
+    } else {
+      console.log(`No matches found`)
+    }
+  } catch (e) {
+    throw new AppError(e + ' (1)')
+  }
+
+  if (issueNumber === null) {
+    try {
+      const githubResponse = await octokit.request(
+        'POST /repos/{owner}/{repo}/issues',
+        {
+          accept: 'application/vnd.github.v3+json',
+          owner: GITHUB_OWNER,
+          repo: GITHUB_REPO,
+          title: issueTitle,
+          labels: [issueLabel],
+        },
+      )
+
+      issueNumber = githubResponse.data.number
+      console.log(`Created issue ${issueNumber} with title ${issueTitle}`)
+    } catch (e) {
+      throw new AppError(e + ' (2)')
+    }
+  }
+
+  try {
+    await octokit.request(
+      'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
+      {
+        accept: 'application/vnd.github.v3+json',
+        owner: GITHUB_OWNER,
+        repo: GITHUB_REPO,
+        issue_number: issueNumber,
+        body: postData.getBody(),
+      },
+    )
+  } catch (e) {
+    throw new AppError(e + ' (3)')
+  }
+
+  console.log('Success')
+  return new Response('Your error report has been received. Thank you!', {
+    status: 200,
+  })
+}
+
+/**
+ * User suggestion - `suggest` command
+ */
+async function handlePostIndexSuggestion(
+  postData: PostData,
+): Promise<Response> {
+  const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+  const issueLabel = 'user suggestion'
+  const issueTitle = sanitizeTitle(postData.message)
+
+  try {
+    const githubResponse = await octokit.request(
+      'POST /repos/{owner}/{repo}/issues',
+      {
+        accept: 'application/vnd.github.v3+json',
+        body: postData.getBody(),
+        owner: GITHUB_OWNER,
+        repo: GITHUB_REPO,
+        title: issueTitle,
+        labels: [issueLabel],
+      },
+    )
+
+    console.log(
+      `Created issue ${githubResponse.data.number} with title "${githubResponse.data.title}"`,
+    )
+  } catch (e) {
+    throw new AppError(e + ' (4)')
+  }
+
+  console.log('Success')
+  return new Response('Your suggestion has been received. Thank you!', {
+    status: 200,
+  })
+}
+
+function handle404(request: Request): Response {
+  throw new AppError('Not found: ' + request.url, 'Not found.', { status: 404 })
+}
+
+function assertContentType(request: Request, contentType: string): void {
+  if (request.headers.get('content-type') !== contentType) {
+    throw new AppError(
+      'Unexpected content type: ' + request.headers.get('content-type'),
+      'Invalid request type.',
+      { status: 415 },
+    )
+  }
+}
+
+function assertRequestMethod(request: Request, expectedMethod: string): void {
+  if (request.method !== expectedMethod) {
+    throw new AppError(
+      'Unexpected request method: ' + request.method,
+      'Invalid request method.',
+      {
+        status: 405,
+        headers: new Headers({
+          Allow: expectedMethod,
+        }),
+      },
+    )
+  }
+}
+
+async function assertNotRateLimited(userIP: string | null): Promise<void> {
+  if (userIP === null) {
+    throw new AppError(
+      'Received a request with no IP address.',
+      'Invalid request.',
+    )
+  }
+
   const rateLimitResultRaw = await RATE_LIMIT.get(
     getRateLimitKey(userIP, 'feedback'),
     { type: 'text' },
@@ -22,8 +297,8 @@ export async function handleRequest(request: Request): Promise<Response> {
     : []
 
   if (rateLimitNew.length > 1) {
-    console.log('Too many requests for IP', userIP)
-    return new Response(
+    throw new AppError(
+      'Too many requests for IP ' + userIP,
       'Thank you for your enthusiasm! Please try submitting again later.',
       { status: 429 },
     )
@@ -33,180 +308,6 @@ export async function handleRequest(request: Request): Promise<Response> {
       getRateLimitKey(userIP, 'feedback'),
       JSON.stringify(rateLimitNew),
       { expirationTtl: 600 },
-    )
-  }
-
-  if (request.method !== 'POST') {
-    console.log('Unexpected request method:', request.method)
-
-    return new Response('Invalid request method.', {
-      status: 405,
-      headers: new Headers({
-        Allow: 'POST',
-      }),
-    })
-  } else if (request.headers.get('content-type') !== 'application/json') {
-    console.log('Unexpected content type:', request.headers.get('content-type'))
-
-    return new Response('JSON body expected.', {
-      status: 415,
-      headers: new Headers({
-        Allow: 'POST',
-      }),
-    })
-  } else {
-    let requestBody = null
-
-    try {
-      requestBody = await request.json()
-    } catch (e) {
-      console.log('Error 0', e)
-      return new Response(e, { status: 400 })
-    }
-
-    console.log('message', requestBody.message)
-    console.log('error', requestBody.error)
-    console.log('user-agent', request.headers.get('user-agent'))
-    console.log('history:\n' + requestBody.history)
-
-    const octokit = new Octokit({ auth: GITHUB_TOKEN })
-
-    if (requestBody.error) {
-      // Bug report - `report` command
-      const errorMessage =
-        'We were unable to record your error report. Please try contacting us by email: support@initiative.sh.'
-      const issueLabel = 'user error report'
-      const issueTitle = sanitizeTitle(requestBody.error)
-      let issueComment = `Message: ${sanitizeInline(requestBody.message)}
-User-agent: ${sanitizeInline(request.headers.get('user-agent'))}
-IP address: ${sanitizeInline(userIP)}`
-
-      if (requestBody.history) {
-        issueComment += `
-
----
-
-${sanitizeBlock(requestBody.history)}`
-      }
-
-      let issueNumber = null
-
-      try {
-        const query = `repo:${OWNER}/${REPO} is:issue is:open label:"${issueLabel}" "${issueTitle}" in:title`
-        console.log('Searching for ' + query)
-
-        const searchResults = await octokit.request('GET /search/issues', {
-          accept: 'application/vnd.github.v3+json',
-          q: query,
-          sort: 'created',
-          per_page: 100,
-        })
-
-        if (searchResults.data.total_count > 0) {
-          for (const issue of searchResults.data.items) {
-            if (issue.title === issueTitle) {
-              issueNumber = issue.number
-              console.log(`Matched issue ${issueNumber}: ${issue.title}`)
-              break
-            }
-          }
-
-          if (issueNumber === null) {
-            console.log(
-              `No exact matches found among ${searchResults.data.total_count} results.`,
-            )
-          }
-        } else {
-          console.log(`No matches found`)
-        }
-      } catch (e) {
-        console.log('Error 1', e)
-        return new Response(errorMessage, { status: 400 })
-      }
-
-      if (issueNumber === null) {
-        try {
-          const githubResponse = await octokit.request(
-            'POST /repos/{owner}/{repo}/issues',
-            {
-              accept: 'application/vnd.github.v3+json',
-              owner: OWNER,
-              repo: REPO,
-              title: issueTitle,
-              labels: [issueLabel],
-            },
-          )
-
-          issueNumber = githubResponse.data.number
-          console.log(`Created issue ${issueNumber} with title ${issueTitle}`)
-        } catch (e) {
-          console.log('Error 2', e)
-          return new Response(errorMessage, { status: 400 })
-        }
-      }
-
-      try {
-        await octokit.request(
-          'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
-          {
-            accept: 'application/vnd.github.v3+json',
-            owner: OWNER,
-            repo: REPO,
-            issue_number: issueNumber,
-            body: issueComment,
-          },
-        )
-      } catch (e) {
-        console.log('Error 3', e)
-        return new Response(errorMessage, { status: 400 })
-      }
-    } else {
-      // Suggestion - `suggest` command
-      const errorMessage =
-        'We were unable to record your suggestion. Please try contacting us by email: support@initiative.sh.'
-      const issueLabel = 'user suggestion'
-      const issueTitle = sanitizeTitle(requestBody.message)
-      let issueBody = `User-agent: ${sanitizeInline(
-        request.headers.get('user-agent'),
-      )}
-IP address: ${sanitizeInline(userIP)}`
-
-      if (requestBody.history) {
-        issueBody += `
-
----
-
-${sanitizeBlock(requestBody.history)}`
-      }
-
-      try {
-        const githubResponse = await octokit.request(
-          'POST /repos/{owner}/{repo}/issues',
-          {
-            accept: 'application/vnd.github.v3+json',
-            body: issueBody,
-            owner: OWNER,
-            repo: REPO,
-            title: issueTitle,
-            labels: [issueLabel],
-          },
-        )
-
-        console.log(
-          `Created issue ${githubResponse.data.number} with title "${githubResponse.data.title}"`,
-        )
-      } catch (e) {
-        console.log('Error 4', e)
-        return new Response(errorMessage, { status: 400 })
-      }
-    }
-
-    console.log('Success')
-    return new Response(
-      'Your ' +
-        (requestBody.error ? 'error report' : 'suggestion') +
-        ' has been received. Thank you!',
-      { status: 200 },
     )
   }
 }
