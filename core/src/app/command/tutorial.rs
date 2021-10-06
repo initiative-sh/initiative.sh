d751777863e0fb2905561efd6683af52507cf25a
Mikkel Paulson <git@email.mikkel.ca>
2021-10-06 10:50:16 -0400

    tutorial step 01 -- WIP

--- core/src/app/command/tutorial.rs
+++ core/src/app/command/tutorial.rs
@@ -1,4 +1,4 @@
-use super::Runnable;
+use super::{CommandAlias, Runnable};
 use crate::app::AppMeta;
 use async_trait::async_trait;
 use std::fmt;
@@ -6,16 +6,47 @@ use std::fmt;
 #[derive(Clone, Debug, PartialEq)]
 pub enum TutorialCommand {
     Introduction,
+    Inn,
 }
 
 #[async_trait(?Send)]
 impl Runnable for TutorialCommand {
-    async fn run(&self, _input: &str, _app_meta: &mut AppMeta) -> Result<String, String> {
-        if matches!(self, TutorialCommand::Introduction) {
-            Ok(include_str!("../../../../data/tutorial/00-intro.md").to_string())
-        } else {
-            unreachable!();
+    async fn run(&self, _input: &str, app_meta: &mut AppMeta) -> Result<String, String> {
+        let (result, next_command) = match self {
+            Self::Introduction => {
+                app_meta.command_aliases.insert(CommandAlias::literal(
+                    "next".to_string(),
+                    "continue the tutorial".to_string(),
+                    Self::Inn.into(),
+                ));
+
+                (
+                    Ok(include_str!("../../../../data/tutorial/00-intro.md").to_string()),
+                    Some(Self::Inn),
+                )
+            }
+            Self::Inn => (
+                Ok(include_str!("../../../../data/tutorial/01-inn.md").to_string()),
+                None,
+            ),
+        };
+        /*
+        Command::parse_input_irrefutable(input, app_meta)
+            .run(input, app_meta)
+            .await
+            .map(|mut output| {
+                output.push_str("\n\n## Tutorial");
+                output
+            })
+        */
+
+        if let Some(command) = next_command {
+            app_meta
+                .command_aliases
+                .insert(CommandAlias::strict_wildcard(command.into()));
         }
+
+        result
     }
 
     fn parse_input(input: &str, _app_meta: &AppMeta) -> (Option<Self>, Vec<Self>) {
@@ -42,6 +73,7 @@ impl fmt::Display for TutorialCommand {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match self {
             Self::Introduction => write!(f, "tutorial"),
+            _ => Ok(()),
         }
     }
 }
