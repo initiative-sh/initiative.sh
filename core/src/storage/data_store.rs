c83b73f3476eaa6f59eff8cb34df7812b88f21b9
Mikkel Paulson <git@email.mikkel.ca>
2021-10-21 13:26:11 -0400

    add MemoryDataStore
    Turns out we really need the MemoryDataStore for unit testing as well as
    integration testing. Trying to work around using NullDataStore for
    everything is actually causing a lot of gaps in test coverage

--- core/src/storage/data_store.rs
+++ core/src/storage/data_store.rs
@@ -4,6 +4,13 @@ use async_trait::async_trait;
 #[derive(Default)]
 pub struct NullDataStore;
 
+#[cfg(test)]
+#[derive(Default)]
+pub struct MemoryDataStore {
+    pub things: std::rc::Rc<std::cell::RefCell<Vec<Thing>>>,
+    pub key_values: std::rc::Rc<std::cell::RefCell<std::collections::HashMap<String, String>>>,
+}
+
 #[async_trait(?Send)]
 impl DataStore for NullDataStore {
     async fn delete_thing_by_uuid(&mut self, _uuid: &Uuid) -> Result<(), ()> {
@@ -28,6 +35,52 @@ impl DataStore for NullDataStore {
 
     async fn delete_value(&mut self, _key: &str) -> Result<(), ()> {
         Err(())
+    }
+}
+
+#[cfg(test)]
+#[async_trait(?Send)]
+impl DataStore for MemoryDataStore {
+    async fn delete_thing_by_uuid(&mut self, uuid: &Uuid) -> Result<(), ()> {
+        let mut things = self.things.borrow_mut();
+
+        if let Some((index, _)) = things
+            .iter()
+            .enumerate()
+            .find(|(_, t)| t.uuid() == Some(uuid))
+        {
+            things.swap_remove(index);
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    async fn get_all_the_things(&self) -> Result<Vec<Thing>, ()> {
+        Ok(self.things.borrow().to_vec())
+    }
+
+    async fn save_thing(&mut self, thing: &Thing) -> Result<(), ()> {
+        let mut things = self.things.borrow_mut();
+        things.push(thing.clone());
+        Ok(())
+    }
+
+    async fn set_value(&mut self, key: &str, value: &str) -> Result<(), ()> {
+        let mut key_values = self.key_values.borrow_mut();
+        key_values.insert(key.to_string(), value.to_string());
+        Ok(())
+    }
+
+    async fn get_value(&self, key: &str) -> Result<Option<String>, ()> {
+        let key_values = self.key_values.borrow();
+        Ok(key_values.get(key).cloned())
+    }
+
+    async fn delete_value(&mut self, key: &str) -> Result<(), ()> {
+        let mut key_values = self.key_values.borrow_mut();
+        key_values.remove(key);
+        Ok(())
     }
 }
 
