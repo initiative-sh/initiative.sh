dccb3fe7337da35b5904ff5a29b1352bb58c1856
Mikkel Paulson <git@email.mikkel.ca>
2021-11-02 14:10:57 -0400

    update lingering references to Rust 2018

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-git ls-files '*.rs' | xargs rustfmt --check --edition 2018
+git ls-files '*.rs' | xargs rustfmt --check --edition 2021
 cargo clippy --workspace -- --deny warnings
 cargo test --workspace
 # (cd web && wasm-pack test --firefox --headless)
