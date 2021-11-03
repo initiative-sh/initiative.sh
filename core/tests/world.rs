73c22f6a8a8656f3be76323f17a8ce1bc8c08c53
Mikkel Paulson <git@email.mikkel.ca>
2021-11-03 09:14:56 -0400

    consolidate world tests
    Most of the npc and place code is the same anyway, so there was a lot of
    duplication between world_npc and world_location.
    
    Also added test coverage for the tutorial without local storage.

--- core/tests/world.rs
+++ core/tests/world.rs
@@ -9,10 +9,15 @@ fn results_are_random() {
         sync_app().command("npc").unwrap(),
         sync_app().command("npc").unwrap(),
     );
+
+    assert_ne!(
+        sync_app().command("building").unwrap(),
+        sync_app().command("building").unwrap(),
+    );
 }
 
 #[test]
-fn generated_content_is_limited_by_species() {
+fn generated_npcs_are_limited_by_species() {
     [
         "dragonborn",
         "dwarf",
@@ -55,50 +60,84 @@ fn generated_content_is_limited_by_species() {
 }
 
 #[test]
+fn generated_locations_are_limited_by_place_type() {
+    ["inn"].iter().for_each(|place_type| {
+        let mut app = sync_app();
+
+        let output = app.command(place_type).unwrap();
+        assert!(
+            output.contains(place_type),
+            "Input: {}\n\nOutput:\n{}",
+            place_type,
+            output,
+        );
+
+        let output = app.command("more").unwrap();
+        assert!(
+            output.matches(place_type).count() >= 11,
+            "Input: {}\n\nOutput:\n{}",
+            place_type,
+            output,
+        );
+
+        let output = app.command("more").unwrap();
+        assert!(
+            output.matches(place_type).count() >= 11,
+            "Input: {}\n\nOutput:\n{}",
+            place_type,
+            output,
+        );
+    });
+}
+
+#[test]
 fn generated_content_is_persisted() {
     let mut app = sync_app();
 
-    {
-        // # Sybil
-        // *elderly human, she/her*
-        //
-        // **Species:** human\
-        // **Gender:** feminine\
-        // **Age:** 64 years\
-        // **Size:** 5'7", 112 lbs (medium)
-        //
-        // _Sybil has not yet been saved. Use ~save~ to save her to your `journal`. For more
-        // suggestions, type ~more~._
-        let generated_output = app.command("npc").unwrap();
+    // # Sybil
+    // *elderly human, she/her*
+    //
+    // **Species:** human\
+    // **Gender:** feminine\
+    // **Age:** 64 years\
+    // **Size:** 5'7", 112 lbs (medium)
+    //
+    // _Sybil has not yet been saved. Use ~save~ to save her to your `journal`. For more
+    // suggestions, type ~more~._
+    let generated_output = app.command("npc").unwrap();
 
-        // Ensure that the primary suggestion matches the generated content.
-        let name = generated_output
+    // Ensure that the primary suggestion matches the generated content.
+    let name = generated_output
+        .lines()
+        .nth(2)
+        .unwrap()
+        .trim_start_matches("# ");
+    let persisted_output = app.command(&format!("load {}", name)).unwrap();
+    assert_eq!(
+        format!("# {}", name),
+        persisted_output.lines().nth(2).unwrap(),
+    );
+    assert_eq!(
+        12,
+        generated_output
             .lines()
-            .nth(2)
-            .unwrap()
-            .trim_start_matches("# ");
-        let persisted_output = app.command(&format!("load {}", name)).unwrap();
-        assert_eq!(
-            format!("# {}", name),
-            persisted_output.lines().nth(2).unwrap(),
-        );
-        assert_eq!(
-            12,
-            generated_output
-                .lines()
-                .zip(persisted_output.lines())
-                .filter(|(generated, _)| !generated.starts_with('_'))
-                .map(|(generated, persisted)| assert_eq!(generated, persisted))
-                .count(),
-            "Generated:\n{}\n\nPersisted:\n{}",
-            generated_output,
-            persisted_output,
-        );
-    }
+            .zip(persisted_output.lines())
+            .filter(|(generated, _)| !generated.starts_with('_'))
+            .map(|(generated, persisted)| assert_eq!(generated, persisted))
+            .count(),
+        "Generated:\n{}\n\nPersisted:\n{}",
+        generated_output,
+        persisted_output,
+    );
+}
 
-    {
-        app.command("npc").unwrap();
+#[test]
+fn more_alias() {
+    let mut app = sync_app();
+    let mut output = app.command("npc").unwrap();
 
+    for i in 1..2 {
+        assert!(output.contains("~more~"), "Iteration {}\n\n{}", i, output);
         // # Alternative suggestions for "person":
         //
         // ~1~ `Jaya` (middle-aged human, she/her)\
@@ -113,12 +152,12 @@ fn generated_content_is_persisted() {
         // ~0~ `Mokosh` (middle-aged half-elf, she/her)
         //
         // _For even more suggestions, type ~more~.
-        let generated_output = app.command("more").unwrap();
+        output = app.command("more").unwrap();
 
         // Ensure that secondary suggestions have also been persisted.
         assert_eq!(
             10,
-            generated_output
+            output
                 .lines()
                 .filter(|line| line.starts_with('~'))
                 .map(|s| {
@@ -131,20 +170,23 @@ fn generated_content_is_persisted() {
                                 .lines()
                                 .nth(2)
                                 .unwrap(),
+                            "Iteration {}",
+                            i,
                         );
                     } else {
                         panic!("Missing ( in \"{}\"", s);
                     }
                 })
                 .count(),
-            "{}",
-            generated_output,
+            "Iteration {}\n\n{}",
+            i,
+            output,
         );
     }
 }
 
 #[test]
-fn numeric_aliases_exist_for_npcs() {
+fn numeric_aliases() {
     let mut app = sync_app();
 
     // Generate a data set to potentially interfere with the one being tested.
@@ -188,7 +230,7 @@ fn numeric_aliases_exist_for_npcs() {
 }
 
 #[test]
-fn save_alias_exists_for_npcs() {
+fn save_alias() {
     let mut app = sync_app();
 
     {
@@ -212,7 +254,7 @@ fn save_alias_exists_for_npcs() {
 }
 
 #[test]
-fn npc_save_alias_does_not_exist_with_invalid_data_store() {
+fn save_alias_does_not_exist_with_invalid_data_store() {
     let mut app = sync_app_with_data_store(NullDataStore::default());
 
     let output = app.command("npc").unwrap();
@@ -284,6 +326,36 @@ fn create_npc_with_custom_attributes() {
 }
 
 #[test]
+fn create_place_with_custom_attributes() {
+    let mut app = sync_app();
+
+    {
+        let output = app.command("an inn called The Prancing Pony").unwrap();
+        assert!(output.contains("# The Prancing Pony\n*inn*"), "{}", output,);
+        assert!(
+            output.contains("has been automatically added to your `journal`."),
+            "{}",
+            output,
+        );
+        assert!(!output.contains("has not yet been saved"), "{}", output);
+        assert!(!output.contains("Alternatives"), "{}", output);
+    }
+
+    {
+        let output = app.command("journal").unwrap();
+        assert!(output.contains("The Prancing Pony"), "{}", output);
+    }
+
+    {
+        let output = app.command("a place named the prancing pony").unwrap_err();
+        assert_eq!(
+            "That name is already in use by `The Prancing Pony`, an inn.",
+            output,
+        );
+    }
+}
+
+#[test]
 fn edit_npc() {
     let mut app = sync_app();
 
@@ -330,16 +402,61 @@ fn edit_npc() {
 }
 
 #[test]
-fn edit_npc_implicitly_saves() {
+fn edit_place() {
     let mut app = sync_app();
 
-    let generated_output = app.command("elf").unwrap();
+    app.command("inn named Hotel California").unwrap();
 
-    let name = generated_output
-        .lines()
-        .nth(2)
-        .unwrap()
-        .trim_start_matches("# ");
+    {
+        let output = app
+            .command("Hotel California is called Heaven Or Hell")
+            .unwrap();
+        assert!(output.contains("# Heaven Or Hell"), "{}", output);
+        assert!(
+            output.ends_with(
+                "_Hotel California was successfully edited. Use `undo` to reverse this._"
+            ),
+            "{}",
+            output,
+        );
+    }
+
+    {
+        let output = app.command("Heaven Or Hell").unwrap();
+        assert!(output.contains("# Heaven Or Hell"), "{}", output);
+    }
+
+    {
+        let output = app.command("undo").unwrap();
+        assert!(output.contains("# Hotel California"), "{}", output);
+        assert!(
+            output.ends_with(
+                "_Successfully undid editing Hotel California. Use `redo` to reverse this._"
+            ),
+            "{}",
+            output,
+        );
+    }
+
+    {
+        let output = app.command("redo").unwrap();
+        assert!(output.contains("# Heaven Or Hell"), "{}", output);
+        assert!(
+            output.ends_with(
+                "_Successfully redid editing Heaven Or Hell. Use `undo` to reverse this._"
+            ),
+            "{}",
+            output,
+        );
+    }
+}
+
+#[test]
+fn edit_implicitly_saves() {
+    let mut app = sync_app();
+
+    let output = app.command("elf").unwrap();
+    let name = output.lines().nth(2).unwrap().trim_start_matches("# ");
 
     {
         let output = app.command("journal").unwrap();
@@ -360,74 +477,35 @@ fn edit_npc_implicitly_saves() {
         let output = app.command("journal").unwrap();
         assert!(output.contains(&name), "{}", output);
     }
-
-    {
-        let output = app.command("undo").unwrap();
-        assert!(output.contains("**Species:** elf"), "{}", output);
-        assert!(
-            output.ends_with(&format!(
-                "_Successfully undid editing {}. Use `redo` to reverse this._",
-                name,
-            )),
-            "{}",
-            output,
-        );
-    }
-
-    {
-        let output = app.command(&name).unwrap();
-        assert!(
-            output.contains(&format!(
-                "_{} has not yet been saved. Use ~save~ to save ",
-                name,
-            )),
-            "{}",
-            output,
-        );
-    }
-
-    {
-        let output = app.command("journal").unwrap();
-        assert!(output.contains("empty"), "{}", output);
-    }
-
-    {
-        let output = app.command("redo").unwrap();
-        assert!(output.contains("**Species:** human"), "{}", output);
-        assert!(
-            output.ends_with(&format!(
-                "_Successfully redid editing {}. Use `undo` to reverse this._",
-                name,
-            )),
-            "{}",
-            output,
-        );
-    }
-
-    {
-        let output = app.command("journal").unwrap();
-        assert!(output.contains(&name), "{}", output);
-    }
 }
 
 #[test]
-fn edit_npc_with_wrong_type() {
+fn edit_with_wrong_type() {
     let mut app = sync_app();
-    app.command("inn named Foo").unwrap();
+    app.command("inn named Place").unwrap();
+    app.command("npc named Person").unwrap();
 
     assert_eq!(
-        "There is no character named \"Foo\".",
-        app.command("Foo is an elf").unwrap_err(),
+        "There is no character named \"Place\".",
+        app.command("Place is an elf").unwrap_err(),
+    );
+    assert_eq!(
+        "There is no place named \"Person\".",
+        app.command("Person is an inn").unwrap_err(),
     );
 
     assert_eq!(
-        "There is no character named \"Bar\".",
-        app.command("Bar is an elf").unwrap_err(),
+        "There is no character named \"Blah\".",
+        app.command("Blah is an elf").unwrap_err(),
+    );
+    assert_eq!(
+        "There is no place named \"Blah\".",
+        app.command("Blah is an inn").unwrap_err(),
     );
 }
 
 #[test]
-fn create_place_with_unknown_words() {
+fn create_with_unknown_words() {
     let mut app = sync_app();
 
     {
@@ -452,7 +530,7 @@ fn create_place_with_unknown_words() {
 }
 
 #[test]
-fn edit_place_with_unknown_words() {
+fn edit_with_unknown_words() {
     let mut app = sync_app();
     app.command("npc named Spot").unwrap();
 
