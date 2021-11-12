a169c79ce9295f5284afa62a5733e304312ac3c1
Mikkel Paulson <git@email.mikkel.ca>
2021-11-12 14:22:18 -0500

    update JS to surface Result objects
    Rather than trying to collapse Result objects in the JS, updated the
    bindings to return that data to Rust for more consistent/reliable
    handling.

--- web/src/data_store.rs
+++ web/src/data_store.rs
@@ -16,14 +16,10 @@ impl initiative_core::DataStore for DataStore {
     }
 
     async fn delete_thing_by_uuid(&mut self, uuid: &Uuid) -> Result<(), ()> {
-        if delete_thing_by_uuid(uuid.to_string().into())
+        delete_thing_by_uuid(uuid.to_string().into())
             .await
-            .is_truthy()
-        {
-            Ok(())
-        } else {
-            Err(())
-        }
+            .map(|_| ())
+            .map_err(|_| ())
     }
 
     async fn edit_thing(&mut self, thing: &Thing) -> Result<(), ()> {
@@ -31,18 +27,27 @@ impl initiative_core::DataStore for DataStore {
     }
 
     async fn get_all_the_things(&self) -> Result<Vec<Thing>, ()> {
-        get_all_the_things().await.into_serde().map_err(|_| ())
+        get_all_the_things()
+            .await
+            .map_err(|_| ())?
+            .into_serde()
+            .map_err(|_| ())
     }
 
     async fn get_thing_by_uuid(&self, uuid: &Uuid) -> Result<Option<Thing>, ()> {
         get_thing_by_uuid(uuid.to_string().into())
             .await
+            .map_err(|_| ())?
             .into_serde()
             .map_err(|_| ())
     }
 
     async fn get_thing_by_name(&self, name: &str) -> Result<Option<Thing>, ()> {
-        get_thing_by_name(name).await.into_serde().map_err(|_| ())
+        get_thing_by_name(name)
+            .await
+            .map_err(|_| ())?
+            .into_serde()
+            .map_err(|_| ())
     }
 
     async fn get_things_by_name_start(
@@ -52,45 +57,28 @@ impl initiative_core::DataStore for DataStore {
     ) -> Result<Vec<Thing>, ()> {
         get_things_by_name_start(name, limit.unwrap_or(usize::MAX))
             .await
+            .map_err(|_| ())?
             .into_serde()
             .map_err(|_| ())
     }
 
     async fn save_thing(&mut self, thing: &Thing) -> Result<(), ()> {
-        if save_thing(JsValue::from_serde(thing).unwrap())
+        save_thing(JsValue::from_serde(thing).unwrap())
             .await
-            .is_truthy()
-        {
-            Ok(())
-        } else {
-            Err(())
-        }
+            .map(|_| ())
+            .map_err(|_| ())
     }
 
     async fn set_value(&mut self, key: &str, value: &str) -> Result<(), ()> {
-        if set_value(key, value).await.is_truthy() {
-            Ok(())
-        } else {
-            Err(())
-        }
+        set_value(key, value).await.map(|_| ()).map_err(|_| ())
     }
 
     async fn get_value(&self, key: &str) -> Result<Option<String>, ()> {
-        let result = get_value(key).await;
-
-        if result.as_bool() == Some(false) {
-            Err(())
-        } else {
-            Ok(result.as_string())
-        }
+        get_value(key).await.map(|v| v.as_string()).map_err(|_| ())
     }
 
     async fn delete_value(&mut self, key: &str) -> Result<(), ()> {
-        if delete_value(key).await.is_truthy() {
-            Ok(())
-        } else {
-            Err(())
-        }
+        delete_value(key).await.map(|_| ()).map_err(|_| ())
     }
 }
 
@@ -98,21 +86,30 @@ impl initiative_core::DataStore for DataStore {
 extern "C" {
     fn health_check() -> JsValue;
 
-    async fn delete_thing_by_uuid(uuid: JsValue) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn delete_thing_by_uuid(uuid: JsValue) -> Result<JsValue, JsValue>;
 
-    async fn get_all_the_things() -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn get_all_the_things() -> Result<JsValue, JsValue>;
 
-    async fn get_thing_by_uuid(uuid: JsValue) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn get_thing_by_uuid(uuid: JsValue) -> Result<JsValue, JsValue>;
 
-    async fn get_thing_by_name(name: &str) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn get_thing_by_name(name: &str) -> Result<JsValue, JsValue>;
 
-    async fn get_things_by_name_start(name: &str, limit: usize) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn get_things_by_name_start(name: &str, limit: usize) -> Result<JsValue, JsValue>;
 
-    async fn save_thing(thing: JsValue) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn save_thing(thing: JsValue) -> Result<JsValue, JsValue>;
 
-    async fn set_value(key: &str, value: &str) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn set_value(key: &str, value: &str) -> Result<JsValue, JsValue>;
 
-    async fn get_value(key: &str) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn get_value(key: &str) -> Result<JsValue, JsValue>;
 
-    async fn delete_value(key: &str) -> JsValue;
+    #[wasm_bindgen(catch)]
+    async fn delete_value(key: &str) -> Result<JsValue, JsValue>;
 }
