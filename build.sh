f6b665cbafdeb4a2b8bae23e2ad08e8219e552bc
Mikkel Paulson <git@email.mikkel.ca>
2021-11-27 10:54:24 -0500

    Revert "announce release after build"
    This reverts commit d8407b5cadee1b84c996faed1afffedb4fb5d40f, as the
    post-build release announcement didn't actually work.

--- build.sh
+++ build.sh
@@ -19,34 +19,3 @@ fi
 wasm-pack build --release
 npm install
 npm run build
-
-if [[ -v DISCORD_RELEASE_WEBHOOK_URL ]] && git diff --name-only HEAD^ | grep -xq data/changelog.md; then
-  read -r -d '' release_announce <<'JS'
-    const https = require('https');
-    if (process.env.message) {
-      const data = JSON.stringify({content: process.env.message});
-      https.request(
-        process.env.DISCORD_RELEASE_WEBHOOK_URL,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length,
-          },
-        },
-        (res) => res.on('data', (d) => {
-          process.stdout.write(d);
-          if (res.statusCode >= 400) {
-            process.exit(1);
-          }
-        })
-      ).write(data);
-    }
-JS
-
-  changelog="$(git diff HEAD^ data/changelog.md | awk '/^+[^+]/{ sub(/^[+ ]+/, ""); sub(/^\* /, "\n- "); printf "%s ", $0 }')"
-  stats="$(git diff --shortstat HEAD^)" 
-  export message="There's a new release on initiative.sh!"$'\n'"$changelog"$'\n\n'"$stats"
-
-  node -e "$release_announce"
-fi
