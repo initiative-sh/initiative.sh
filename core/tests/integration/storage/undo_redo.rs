dad776933d0bc46f05928e316cd4c6d3c7ff9515
Mikkel Paulson <git@email.mikkel.ca>
2021-12-14 14:31:53 -0500

    add test coverage of undo/redo autocomplete
    Turns out that I never wrote tests for autocomplete descriptions of undo
    and redo, which is a pity because I've been complemented on this little
    piece of polish in the past. Added coverage before embarking on a
    refactor.

--- core/tests/integration/storage/undo_redo.rs
+++ core/tests/integration/storage/undo_redo.rs
@@ -1,10 +1,13 @@
 use crate::common::{sync_app, SyncApp};
+use std::borrow::Cow;
 
 fn undo_redo_test(
     app: &mut SyncApp,
     test: &str,
     change: &str,
+    expect_undo_autocomplete: &str,
     expect_undo_message: &str,
+    expect_redo_autocomplete: &str,
     expect_redo_message: &str,
 ) -> [Result<String, String>; 4] {
     let a = app.command(test);
@@ -12,6 +15,11 @@ fn undo_redo_test(
     let b = app.command(test);
 
     {
+        assert_eq!(
+            Some(&(Cow::from("undo"), Cow::from(expect_undo_autocomplete))),
+            app.autocomplete("undo").first(),
+        );
+
         let output = app.command("undo").unwrap();
         assert_eq!(
             expect_undo_message,
@@ -24,6 +32,11 @@ fn undo_redo_test(
     let c = app.command(test);
 
     {
+        assert_eq!(
+            Some(&(Cow::from("redo"), Cow::from(expect_redo_autocomplete))),
+            app.autocomplete("redo").first(),
+        );
+
         let output = app.command("redo").unwrap();
         assert_eq!(
             expect_redo_message,
@@ -41,7 +54,16 @@ fn undo_redo_test(
 #[test]
 fn nothing() {
     assert_eq!("Nothing to undo.", sync_app().command("undo").unwrap_err());
+    assert_eq!(
+        Some(&(Cow::from("undo"), Cow::from("Nothing to undo."))),
+        sync_app().autocomplete("undo").first(),
+    );
+
     assert_eq!("Nothing to redo.", sync_app().command("redo").unwrap_err());
+    assert_eq!(
+        Some(&(Cow::from("redo"), Cow::from("Nothing to redo."))),
+        sync_app().autocomplete("redo").first(),
+    );
 }
 
 #[test]
@@ -50,7 +72,9 @@ fn create_and_save() {
         &mut sync_app(),
         "load Potato Johnson",
         "character named Potato Johnson",
+        "undo creating Potato Johnson",
         "Successfully undid creating Potato Johnson. Use `redo` to reverse this.",
+        "redo creating Potato Johnson",
         "Successfully redid creating Potato Johnson. Use `undo` to reverse this.",
     );
     assert_ne!(a, b);
@@ -67,7 +91,9 @@ fn delete() {
         &mut app,
         "load Potato Johnson",
         "delete Potato Johnson",
+        "undo deleting Potato Johnson",
         "Successfully undid deleting Potato Johnson. Use `redo` to reverse this.",
+        "redo deleting Potato Johnson",
         "Successfully redid deleting Potato Johnson. Use `undo` to reverse this.",
     );
     assert_ne!(a, b);
@@ -84,7 +110,9 @@ fn edit() {
         &mut app,
         "load Potato Johnson",
         "Potato Johnson is an elf",
+        "undo editing Potato Johnson",
         "Successfully undid editing Potato Johnson. Use `redo` to reverse this.",
+        "redo editing Potato Johnson",
         "Successfully redid editing Potato Johnson. Use `undo` to reverse this.",
     );
     assert_ne!(a, b);
@@ -98,7 +126,9 @@ fn set_key_value() {
         &mut sync_app(),
         "now",
         "+1d",
+        "undo changing the time",
         "Successfully undid changing the time. Use `redo` to reverse this.",
+        "redo changing the time",
         "Successfully redid changing the time. Use `undo` to reverse this.",
     );
     assert_ne!(a, b);
