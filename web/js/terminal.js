d62876a545f6e4f2f81848e455824e02c38ff115
Mikkel Paulson <git@email.mikkel.ca>
2022-12-05 20:07:22 -0500

    Add MIT license to terminal.js
    Supporting an eventual open-sourcing of the terminal component, added
    the MIT license inline to the module.

--- web/js/terminal.js
+++ web/js/terminal.js
@@ -1,3 +1,31 @@
+/*
+ * Copyright © 2022 Mikkel Paulson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * -----
+ *
+ * Note that this module uses the MIT license, whereas the rest of the
+ * application uses GPL 3.0. This is a deliberate choice, because this module is
+ * intended to eventually be released as a standalone npm package.
+ */
+
 import autoComplete from "@tarekraafat/autocomplete.js"
 import { marked } from "marked"
 
