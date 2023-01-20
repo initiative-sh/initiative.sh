1b4d21d511d2a9eb65c3405abca32d17c0129242
Mikkel Paulson <git@email.mikkel.ca>
2023-01-20 09:00:11 -0500

    Link GitHub profiles in changelog

--- macros/src/changelog.rs
+++ macros/src/changelog.rs
@@ -1,4 +1,5 @@
 use proc_macro::{Literal, TokenStream, TokenTree};
+use std::borrow::Cow;
 
 pub fn run(input: TokenStream) -> Result<TokenStream, String> {
     if !input.is_empty() {
@@ -6,14 +7,22 @@ pub fn run(input: TokenStream) -> Result<TokenStream, String> {
     }
 
     let changelog = include_str!("../../data/changelog.md");
-    let token_tree: TokenTree = Literal::string(
-        changelog
-            .split_inclusive("\n*")
-            .take(10)
-            .collect::<String>()
-            .trim_end_matches(&['\n', '*'][..]),
-    )
-    .into();
 
-    Ok(token_tree.into())
+    let top_items = changelog
+        .split_inclusive("\n*")
+        .take(10)
+        .collect::<String>();
+
+    Ok(TokenTree::from(Literal::string(
+        linkify(&top_items).trim_end_matches(&['\n', '*'][..]),
+    ))
+    .into())
+}
+
+pub fn linkify(input: &str) -> Cow<str> {
+    let re = regex::Regex::new(r"@([\w]+)").unwrap();
+    re.replace_all(
+        input,
+        "<a href=\"https://github.com/$1\" target=\"_blank\">@$1</a>",
+    )
 }
