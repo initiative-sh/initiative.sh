ef3c624e027edca0290c7b25026995c2e69e66e1
Chris Renfrow <dev@chrisrenfrow.me>
2023-02-22 13:20:50 -0800

    Add ListGenerator and apply in vocabulary.rs

--- core/src/world/vocabulary.rs
+++ core/src/world/vocabulary.rs
@@ -1,97 +1,89 @@
 use rand::Rng;
 
+#[rustfmt::skip]
 pub fn adjective(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const ADJECTIVES: &[&str] = &[
+    ListGenerator(&[
         "Blue", "Bronze", "Brown", "Burgundy", "Driven", "Enchanted", "Gold", "Green", "Grey",
         "Grouchy", "Hallowed", "Happy", "Hidden", "Hungry", "Jovial", "Lone", "Lost", "Lucky",
         "Merry", "Moody", "Morose", "Orange", "Purple", "Red", "Silent", "Silver", "Thirsty",
         "Wasted", "Wild",
-    ];
-    ADJECTIVES[rng.gen_range(0..ADJECTIVES.len())]
+    ]).gen(rng)
 }
 
 pub fn cardinal_direction(rng: &mut impl Rng) -> &'static str {
-    const CARDINAL_DIRS: &[&str] = &["North", "South", "East", "West"];
-    CARDINAL_DIRS[rng.gen_range(0..CARDINAL_DIRS.len())]
+    ListGenerator(&["North", "South", "East", "West"]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn enemy(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const ENEMIES: &[&str] = &[
+    ListGenerator(&[
         "Angel", "Bandit", "Brigand", "Centaur", "Chimera", "Demon", "Devil", "Dragon", "Fairy",
         "Ghost", "Giant", "Goblin", "Gorgon", "Gremlin", "Hag", "Harpy", "Hydra", "Imp", "Kappa",
         "Lich", "Manticore", "Minotaur", "Necromancer", "Oni", "Orc", "Peryton", "Pirate", "Roc",
         "Satyr", "Seraph", "Siren", "Sorcerer", "Sphinx", "Thief", "Trickster", "Troll", "Unicorn",
         "Vampire", "Werewolf", "Witch", "Wyvern", "Zombie",
-    ];
-    ENEMIES[rng.gen_range(0..ENEMIES.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn food(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const FOODS: &[&str] = &[
+    ListGenerator(&[
         "Barley", "Barrel", "Beef", "Beer", "Bread", "Cask", "Cheese", "Hop", "Keg", "Malt",
         "Mead", "Meat", "Mutton", "Pint", "Pork", "Potatoes", "Rye", "Tun", "Veal", "Venison",
         "Vine",
-    ];
-    FOODS[rng.gen_range(0..FOODS.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn gem(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const GEMS: &[&str] = &[
+    ListGenerator(&[
         "Amber", "Agate", "Amethyst", "Aquamarine", "Beryl", "Citrine", "Diamond", "Emerald",
         "Opal", "Quartz", "Sapphire", "Topaz"
-    ];
-    GEMS[rng.gen_range(0..GEMS.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 fn person(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const PEOPLE: &[&str] = &[
+    ListGenerator(&[
         "Father", "Mother", "Parent", "Sibling", "Hunter", "Emperor", "Empress", "Warrior",
         "Sage", "Ancestor"
-    ];
-    PEOPLE[rng.gen_range(0..PEOPLE.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn profession(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const PROFESSIONS: &[&str] = &[
+    ListGenerator(&[
         "Adventurer", "Baker", "Beggar", "Blacksmith", "Brewer", "Bricklayer", "Builder",
         "Butcher", "Carpenter", "Conjurer", "Cooper", "Diviner", "Enchanter", "Evoker", "Farrier",
         "Ferryman", "Fisherman", "Glazier", "Illusionist", "Knight", "Mage", "Magician", "Mason",
         "Miller", "Plumber", "Porter", "Printer", "Roper", "Sailor", "Shipwright", "Smith",
         "Soldier", "Waterman", "Warrior", "Wizard",
-    ];
-    PROFESSIONS[rng.gen_range(0..PROFESSIONS.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn symbol(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const SYMBOLS: &[&str] = &[
+    ListGenerator(&[
         "Abbey", "Anchor", "Anvil", "Arrow", "Axe", "Belfry", "Bell", "Book", "Buckle", "Cap",
         "Castle", "Column", "Crescent", "Crown", "Drum", "Feather", "Foil", "Hammer", "Harp",
         "Harrow", "Helmet", "Horseshoe", "Key", "Lance", "Lance", "Locket", "Mace", "Mill",
         "Mitre", "Moon", "Nail", "Oar", "Phalactary", "Rake", "Rook", "Scale", "Sceptre", "Scythe",
         "Ship", "Shovel", "Spear", "Spur", "Star", "Steeple", "Sun", "Sword", "Thunderbolt",
         "Tower", "Trumpet", "Wand", "Wheel",
-    ];
-    SYMBOLS[rng.gen_range(0..SYMBOLS.len())]
+    ]).gen(rng)
 }
 
 pub fn any_animal(rng: &mut impl Rng) -> &'static str {
-    match rng.gen_range(0..1) {
+    match rng.gen_range(0..=1) {
         0 => land_animal(rng),
         1 => coastal_animal(rng),
         _ => unreachable!(),
     }
 }
 
+#[rustfmt::skip]
 pub fn land_animal(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const LAND_ANIMALS: &[&str] = &[
+    ListGenerator(&[
         "Antelope", "Ape", "Baboon", "Badger", "Bat", "Bear", "Beaver", "Bee", "Beetle", "Boar",
         "Camel", "Cat", "Cow", "Deer", "Dog", "Donkey", "Dove", "Dragonfly", "Duck", "Eagle",
         "Elephant", "Elk", "Ermine", "Fox", "Frog", "Goat", "Goose", "Hare", "Hart", "Hawk",
@@ -100,16 +92,23 @@ pub fn land_animal(rng: &mut impl Rng) -> &'static str {
         "Rabbit", "Ram", "Rat", "Raven", "Salamander", "Scorpion", "Sheep", "Snake", "Spider",
         "Squirrel", "Stag", "Stoat", "Stork", "Swan", "Tiger", "Toad", "Tortoise",
         "Turkey", "Turtle", "Unicorn", "Vulture", "Weasel", "Wolf",
-    ];
-    LAND_ANIMALS[rng.gen_range(0..LAND_ANIMALS.len())]
+    ]).gen(rng)
 }
 
+#[rustfmt::skip]
 pub fn coastal_animal(rng: &mut impl Rng) -> &'static str {
-    #[rustfmt::skip]
-    const COASTAL_ANIMALS: &[&str] = &[
+    ListGenerator(&[
         "Cormorant", "Crab", "Dolphin", "Herring", "Mermaid", "Octopus", "Osprey", "Otter",
         "Pelican", "Perch", "Salmon", "Seagull", "Seal", "Shark", "Starfish", "Squid", "Whale",
-        "Whelk",
-    ];
-    COASTAL_ANIMALS[rng.gen_range(0..COASTAL_ANIMALS.len())]
+        "Whelk"
+    ])
+    .gen(rng)
+}
+
+pub struct ListGenerator(pub &'static [&'static str]);
+
+impl ListGenerator {
+    pub fn gen(&self, rng: &mut impl Rng) -> &'static str {
+        self.0[rng.gen_range(0..self.0.len())]
+    }
 }
