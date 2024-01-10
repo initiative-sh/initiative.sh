fd0e403360ba6e4c0ce675efb2d0ed47f9861911
Mikkel Paulson <git@email.mikkel.ca>
2024-01-10 12:23:29 -0500

    fix clippy error

--- core/src/world/mod.rs
+++ core/src/world/mod.rs
@@ -5,7 +5,7 @@ pub mod place;
 pub use command::{ParsedThing, WorldCommand};
 pub use demographics::Demographics;
 pub use field::Field;
-pub use npc::{Npc, NpcRelations, Uuid as NpcUuid};
+pub use npc::{Npc, NpcRelations};
 pub use place::{Place, PlaceRelations, Uuid as PlaceUuid};
 pub use thing::{Thing, ThingRelations};
 
